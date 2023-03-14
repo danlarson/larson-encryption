13d94c177691ea946ebc5acb43d30757f6e53d23
Daniel Larson <me@danlarson.com>
2023-03-14 11:02:50 -0600

    Fixes aes dispose/flush

--- src/LarsonTech.Encryption/AesEncryptor.cs
+++ src/LarsonTech.Encryption/AesEncryptor.cs
@@ -1,10 +1,11 @@
 ﻿namespace LarsonTech.Encryption;
 
 /// <summary>
-/// A general purpose AES Encryption class. 
+///     A general purpose AES Encryption class.
 /// </summary>
 public abstract class AesEncryptor : IBinaryEncryptor
 {
+    public const int KeyLength = 32;
     private readonly byte[] _key;
 
     protected AesEncryptor(byte[] key)
@@ -12,7 +13,15 @@ public abstract class AesEncryptor : IBinaryEncryptor
         _key = key;
     }
 
-    public const int KeyLength = 32;
+    byte[] IBinaryEncryptor.DecryptBytes(byte[] encryptedValue)
+    {
+        return Decrypt(encryptedValue);
+    }
+
+    byte[] IBinaryEncryptor.EncryptBytes(byte[] data)
+    {
+        return Encrypt(data);
+    }
 
     protected static byte[] CreateCryptographicKey()
     {
@@ -41,8 +50,7 @@ public abstract class AesEncryptor : IBinaryEncryptor
 
     public static byte[] Encrypt(byte[] encryptionKey, byte[] toEncryptBytes)
     {
-        if (encryptionKey == null || encryptionKey.Length == 0) 
-            throw new ArgumentException("Encryption key is out of range", nameof(encryptionKey));
+        if (encryptionKey == null || encryptionKey.Length == 0) throw new ArgumentException("encryptionKey");
 
         using var aes = Aes.Create();
         aes.Key = encryptionKey;
@@ -51,19 +59,39 @@ public abstract class AesEncryptor : IBinaryEncryptor
 
         aes.GenerateIV();
         var iv = aes.IV;
+        var encrypter = aes.CreateEncryptor(aes.Key, iv);
+        try
+        {
+            MemoryStream? cipherStream = null;
+            try
+            {
+                cipherStream = new MemoryStream();
+                CryptoStream tCryptoStream = null;
+                try
+                {
+                    tCryptoStream = new CryptoStream(cipherStream, encrypter, CryptoStreamMode.Write);
+                    using var tBinaryWriter = new BinaryWriter(tCryptoStream);
+                    //Prepend IV to data
+                    cipherStream.Write(iv, 0, iv.Length);
+                    tBinaryWriter.Write(toEncryptBytes);
+                    tCryptoStream.FlushFinalBlock();
+                }
+                finally
+                {
+                    tCryptoStream?.Dispose();
+                }
 
-        using var encryptor = aes.CreateEncryptor(aes.Key, iv);
-        using var cipherStream = new MemoryStream();
-        using var tCryptoStream = new CryptoStream(cipherStream, encryptor, CryptoStreamMode.Write);
-        using var tBinaryWriter = new BinaryWriter(tCryptoStream);
-
-        //Prepend IV to data
-        cipherStream.Write(iv, 0, iv.Length);
-        tBinaryWriter.Write(toEncryptBytes);
-        tBinaryWriter.Flush();
-        tCryptoStream.FlushFinalBlock();
-
-        return cipherStream.ToArray();
+                return cipherStream.ToArray();
+            }
+            finally
+            {
+                cipherStream?.Dispose();
+            }
+        }
+        finally
+        {
+            encrypter.Dispose();
+        }
     }
 
     public byte[] Decrypt(byte[]? input)
@@ -71,7 +99,7 @@ public abstract class AesEncryptor : IBinaryEncryptor
         if (input == null || input.Length == 0)
             return Array.Empty<byte>();
 
-        return Decrypt(this._key, input);
+        return Decrypt(_key, input);
     }
 
     public static byte[] Decrypt(byte[] encryptionKey, byte[]? input)
@@ -84,31 +112,36 @@ public abstract class AesEncryptor : IBinaryEncryptor
         aes.Mode = CipherMode.CBC;
         aes.Padding = PaddingMode.PKCS7;
 
+
         //get first 16 bytes of IV and use it to decrypt
         var iv = new byte[16];
         Array.Copy(input, 0, iv, 0, iv.Length);
 
-        using var ms = new MemoryStream();
+        MemoryStream? ms = null;
+        try
+        {
+            ms = new MemoryStream();
+            try
+            {
+                var cs = new CryptoStream(ms, aes.CreateDecryptor(aes.Key, iv), CryptoStreamMode.Write);
+                using var binaryWriter = new BinaryWriter(cs);
+                // Decrypt Cipher Text from Message
+                binaryWriter.Write(
+                    input,
+                    iv.Length,
+                    input.Length - iv.Length
+                );
+            }
+            finally
+            {
+                ms.Dispose();
+            }
 
-        var cs = new CryptoStream(ms, aes.CreateDecryptor(aes.Key, iv), CryptoStreamMode.Write);
-        using var binaryWriter = new BinaryWriter(cs);
-        //Decrypt Cipher Text from Message
-        binaryWriter.Write(
-            input,
-            iv.Length,
-            input.Length - iv.Length
-            );
-        binaryWriter.Flush();
-        return ms.ToArray();
-    }
-
-    byte[] IBinaryEncryptor.DecryptBytes(byte[] encryptedValue)
-    {
-        return this.Decrypt(encryptedValue);
-    }
-
-    byte[] IBinaryEncryptor.EncryptBytes(byte[] data)
-    {
-        return this.Encrypt(data);
+            return ms.ToArray();
+        }
+        finally
+        {
+            ms?.Dispose();
+        }
     }
 }
